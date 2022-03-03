e3ee18ec4425e3a72a7c746df79467f8c758c073
Luca Argentieri <Lucaargentieri-98@hotmail.it>
2022-03-03 15:03:04 +0100

    update

--- next.config.js
+++
@@ -1,6 +0,0 @@
-const ghPages = process.env.DEPLOY_TARGET === "gh-pages";
-const withNextOptimizedImages = require("next-optimized-images");
-
-module.exports = withNextOptimizedImages({
-  target: "serverless",
-});
