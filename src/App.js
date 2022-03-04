3f5aaee47a14c64efc0c50b867060a030dd14fd9
Luca Argentieri <Lucaargentieri-98@hotmail.it>
2022-03-04 14:52:45 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <p>
+        I'm working at{" "}
+        <a href="http://display.design/" target="_blank" rel="noopener noreferrer">
+          display.design
+        </a>{" "}
+        as front-end developer.
+      </p>
     </div>
   );
 }
